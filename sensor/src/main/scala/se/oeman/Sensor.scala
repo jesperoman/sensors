ea0832edf69600e8f9385757f880b4eb62c41fa0
Jesper Öman <jo@magine.com>
2016-03-25 20:05:07 +0100

    Parametrize remote ip

--- sensor/src/main/scala/se/oeman/Sensor.scala
+++ sensor/src/main/scala/se/oeman/Sensor.scala
@@ -3,10 +3,12 @@ package se.oeman
 import akka.actor.{ActorSystem, Props}
 
 object Sensor extends App {
+  val remoteIp = args(0)
+  println(s"Sending temperature to $remoteIp")
   println("Creating actor system...")
   val system = ActorSystem.create("SensorSystem")
   println("Creating receiving actor...")
-  val receiver = system.actorSelection("akka.tcp://SensorReceiverSystem@127.0.0.1:2552/user/receiver")
+  val receiver = system.actorSelection(s"akka.tcp://SensorReceiverSystem@$remoteIp:2552/user/receiver")
 //  val receiver = system.actorOf(Props.create(classOf[SensorReceiverActor]))
   println("Instantiating sensor actor...")
   val sender = system.actorOf(Props.create(classOf[JNACallbackActor], receiver))

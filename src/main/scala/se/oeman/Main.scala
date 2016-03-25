025040698d05571eac5d62bc52b3bf1697beb94f
Jesper Öman <jo@magine.com>
2016-03-25 10:11:39 +0100

    Correct system name

--- src/main/scala/se/oeman/Main.scala
+++ src/main/scala/se/oeman/Main.scala
@@ -6,7 +6,7 @@ object Main extends App {
   println("Creating actor system...")
   val system = ActorSystem.create("SensorSystem")
   println("Creating receiving actor...")
-  val receiver = system.actorSelection("akka.tcp://actorSystemName@127.0.0.1:2552/SensorReceiverSystem/SensorReceiverActor")
+  val receiver = system.actorSelection("akka.tcp://SensorReceiverSystem@127.0.0.1:2552/SensorReceiverSystem/SensorReceiverActor")
 //  val receiver = system.actorOf(Props.create(classOf[SensorReceiverActor]))
   println("Instantiating sensor actor...")
   val sender = system.actorOf(Props.create(classOf[JNACallbackActor], receiver))

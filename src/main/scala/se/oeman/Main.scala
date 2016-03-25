2f768c0387914728987d3be185a045964c22fc26
Jesper Öman <jo@magine.com>
2016-03-25 09:46:28 +0100

    Send remote

--- src/main/scala/se/oeman/Main.scala
+++ src/main/scala/se/oeman/Main.scala
@@ -6,7 +6,8 @@ object Main extends App {
   println("Creating actor system...")
   val system = ActorSystem.create("SensorSystem")
   println("Creating receiving actor...")
-  val receiver = system.actorOf(Props.create(classOf[SensorReceiverActor]))
+  val receiver = system.actorSelection("akka.tcp://actorSystemName@127.0.0.1:2552/SensorReceiverSystem/SensorReceiverActor")
+//  val receiver = system.actorOf(Props.create(classOf[SensorReceiverActor]))
   println("Instantiating sensor actor...")
   val sender = system.actorOf(Props.create(classOf[JNACallbackActor], receiver))
 }
